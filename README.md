83d9c9ed7e5109ec2a9310f2319ccf13ce50512e
Phuc Tran <phuctran12032004@gmail.com>
2025-05-24 15:10:29 +0700

    auto install readmeai, add ci/cd exe

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Just a script for auto generate README.md file to your repository
+Step 1: Clone your repository into your machine
--- main.py
+++ main.py
@@ -5,14 +5,22 @@ import subprocess
 # repository_path and readme_saved_path should be set to your actual paths
 # Exaple: 
 # repository_path: "C:\Users\your\real\path\to\your\repository"
-# readme_saved_path: "C:\Users\your\real\path\to\save\README.md"
+# readme_saved_path: "C:\Users\your\real\path\to\save"
 
-repository_path = r"replace your path here"
-readme_saved_path = r"replace your path here"
+repository_path = r""
+readme_saved_path = r""
 
 if(not repository_path or not readme_saved_path):
-    input("Enter the path to your local repository: ", repository_path)
-    input("Enter the path to save the generated README.md: ", readme_saved_path)
+    repository_path = input("Enter the path to your local repository: ")
+    readme_saved_path = input("Enter the path to save the generated README.md: ")
+
+readme_saved_path = readme_saved_path if readme_saved_path.endswith('.md') else readme_saved_path + '/README.md'    
+
+try:
+    subprocess.run("pip show readmeai", check=True, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+except subprocess.CalledProcessError:
+    print("readmeai not installed, installing readmeai...")
+    subprocess.run("pip install readmeai", check=True, shell=True)
 
 command = f'readmeai -r "{repository_path}" -o "{readme_saved_path}"'
 
